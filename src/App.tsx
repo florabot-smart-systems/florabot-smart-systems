237f81fb2ede8a527caea65c124433e543e8c371
Mohtasim Hadi Rafi <mzr0167@auburn.edu>
2024-09-27 11:35:00 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import HomeSection from './components/HomeSection';
-import PathFollowingSection from './components/PathFollowingSection';
-import PlantCountingSection from './components/PlantCountingSection';
-import QualityAssessmentSection from './components/QualityAssessmentSection';
-import TeamSection from './components/TeamSection';
-import Footer from './components/Footer';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme'; 
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Navbar />
-      <div id="home">
-        <HomeSection />
-      </div>
-      <div id="path-following">
-        <PathFollowingSection />
-      </div>
-      <div id="plant-counting">
-        <PlantCountingSection />
-      </div>
-      <div id="quality-assessment">
-        <QualityAssessmentSection />
-      </div>
-      <div id="team">
-        <TeamSection />
-      </div>
-      <Footer />
-    </ThemeProvider>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
-};
+}
 
 export default App;
--- src/theme.ts
+++
@@ -1,9 +0,0 @@
-import { createTheme } from '@mui/material/styles';
-
-const theme = createTheme({
-    typography: {
-      fontFamily: '"Times New Roman", Times, serif',
-    },
-  });
-
-export default theme;
